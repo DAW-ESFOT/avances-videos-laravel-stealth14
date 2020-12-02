7d457c89533acfe4981fde4e9c8bd4da1f2c9a33
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-12-02 14:57:14 -0500

    #8 rutas para article

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,25 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('articles', function () {
+    return Article::all();
+});
+
+Route::get('articles/{id}', function ($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function (Request $request) {
+    return Article::create($request->all());
+});
+
+Route::put('articles/{id}', function (Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+
+Route::delete('articles/{id}', function ($id) {
+    Article::find($id)->delete();
+    return 204;
 });
