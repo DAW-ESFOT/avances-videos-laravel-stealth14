e9ba353a1580e05182ea9e6e9df6c0fe802aa13b
Ronny Cajas <ronny.cajas@epn.edu.ec>
2021-01-25 23:34:55 -0500

    Inicio de sesión , registro y protección de rutas con JWT

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -14,8 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('articles', 'ArticleController@index'); 
-Route::get('articles/{article}', 'ArticleController@show'); 
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
 Route::post('articles', 'ArticleController@store');
-Route::put('articles/{article}', 'ArticleController@update'); 
+Route::put('articles/{article}', 'ArticleController@update');
 Route::delete('articles/{article}', 'ArticleController@delete');
+
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@authenticate');
+Route::get('articles', 'ArticleController@index');
+Route::group(['middleware' => ['jwt.verify']], function () {
+  Route::get('user', 'UserController@getAuthenticatedUser');
+  Route::get('articles/{article}', 'ArticleController@show');
+  Route::post('articles', 'ArticleController@store');
+  Route::put('articles/{article}', 'ArticleController@update');
+  Route::delete('articles/{article}', 'ArticleController@delete');
+});
